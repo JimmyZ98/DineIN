ff44d4746e491cdd34c674fbb5119aecb3530857
JimmyZ98 <sam.jimmy.zhen@gmail.com>
2023-09-11 17:22:07 -0400

    added routing/page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
+import Home from "./pages/Home/Home";
+import Menu from "./pages/Menu/Menu";
+import Cart from "./pages/Cart/Cart";
+import MyOrder from "./pages/MyOrder/MyOrder";
 
 function App() {
   return (
     <>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/myorder" element={<MyOrder />} />
+      </Routes>
     </>
   );
 }
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { AiFillHome } from "react-icons/ai";
 import { MdRestaurantMenu } from "react-icons/md";
 import { FaCartShopping } from "react-icons/fa6";
@@ -8,22 +9,22 @@ export default function NavBar() {
   return (
     <nav className="fixed bottom-0 left-0 w-full bg-amber-400">
       <div className="flex">
-        <div className="nav-button">
+        <Link to="/" className="nav-button">
           <AiFillHome />
-          <a>Home</a>
-        </div>
-        <div className="nav-button">
+          <span>Home</span>
+        </Link>
+        <Link to="/menu" className="nav-button">
           <MdRestaurantMenu />
-          <a>Menu</a>
-        </div>
-        <div className="nav-button">
+          <span>Menu</span>
+        </Link>
+        <Link to="/cart" className="nav-button">
           <FaCartShopping />
-          <a>Cart</a>
-        </div>
-        <div className="nav-button">
+          <span>Cart</span>
+        </Link>
+        <Link to="/myorder" className="nav-button">
           <RiFileList3Fill />
-          <a>My Order</a>
-        </div>
+          <span>My Order</span>
+        </Link>
       </div>
     </nav>
   );

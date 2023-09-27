3af51fb4a05e61b67e8f848fd9955150c1f60c1f
JimmyZ98 <sam.jimmy.zhen@gmail.com>
2023-09-27 18:30:24 -0400

    created menu nav bar for sections, defined interface for menu data props

--- src/components/MenuNav/MenuNav.tsx
+++ src/components/MenuNav/MenuNav.tsx
@@ -1,14 +1,40 @@
 import React from "react";
 
-export default function MenuNav() {
+interface menuDataProps {
+  menuData: {
+    section: string;
+    items: { name: string; price: number }[];
+  }[];
+}
+
+export default function MenuNav({ menuData }: menuDataProps) {
   return (
     <div>
-      <ul>
-        <li>Promotions</li>
-        <li>Lunch Specials</li>
-        <li>Daily Specials</li>
-        <li></li>
+      <ul className="flex flex-row overflow-x-auto h-10">
+        <li className="menu-nav-list">Promotions</li>
+        <li className="menu-nav-list">Lunch Specials</li>
+        <li className="menu-nav-list">Daily Specials</li>
+        <li className="menu-nav-list">Soups</li>
+        <li className="menu-nav-list">Salads</li>
+        <li className="menu-nav-list">Appetizers</li>
+        <li className="menu-nav-list">House Rolls</li>
+        <li className="menu-nav-list">Hand Rolls</li>
+        <li className="menu-nav-list">A La Carte</li>
+        <li className="menu-nav-list">Sushi Sets</li>
+        <li className="menu-nav-list">Maki Sets</li>
+        <li className="menu-nav-list">Sashimi Sets</li>
+        <li className="menu-nav-list">Poke Bowls</li>
+        <li className="menu-nav-list">Rice</li>
+        <li className="menu-nav-list">Bento Boxes</li>
+        <li className="menu-nav-list">Ramen & Udon</li>
+        <li className="menu-nav-list">Chinese Food</li>
+        <li className="menu-nav-list">Dinner Specials</li>
+        <li className="menu-nav-list">Party Trays</li>
+        <li className="menu-nav-list">Beverages</li>
+        <li className="menu-nav-list">Alcohol</li>
+        <li className="menu-nav-list">Desserts</li>
       </ul>
+      <div>{menuData[0].section}</div>
     </div>
   );
 }
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,11 +1,50 @@
 import React from "react";
 import MenuNav from "../../components/MenuNav/MenuNav";
+import { FaSearch } from "react-icons/fa";
+
+// interface menuDataProps {
+//   menuProps: { section: string; items: { name: string; price: number }[] }[];
+// }
+
+const menuData = [
+  {
+    section: "Promotions",
+    items: [
+      { name: "Salmon Avocado Roll", price: 4 },
+      { name: "Yam & Avocado Roll", price: 3.5 },
+    ],
+  },
+  {
+    section: "Lunch Specials",
+    items: [
+      { name: "s1 - Cali & Salmon Rolls", price: 10 },
+      { name: "s2 - Cali & Dynamite Rolls", price: 10 },
+    ],
+  },
+  {
+    section: "Daily Specials",
+    items: [
+      { name: "All Day Sepcial", price: 14 },
+      { name: "Combo 1", price: 8.25 },
+    ],
+  },
+  {
+    section: "Appetizers",
+    items: [
+      { name: "Edamame", price: 5.5 },
+      { name: "Veg Tempura", price: 6.25 },
+    ],
+  },
+];
 
 export default function Menu() {
   return (
-    <div>
-      <h1>Menu</h1>
-      <MenuNav />
-    </div>
+    <section>
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold">Menu</h1>
+        <FaSearch className="text-orange" />
+      </div>
+      <MenuNav menuData={menuData} />
+    </section>
   );
 }

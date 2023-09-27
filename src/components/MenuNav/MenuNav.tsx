978ec343f83838a33cb8c026d82628cd0d86c4ce
JimmyZ98 <sam.jimmy.zhen@gmail.com>
2023-09-27 16:52:57 -0400

    styling changes

--- src/components/MenuNav/MenuNav.tsx
+++ src/components/MenuNav/MenuNav.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function MenuNav() {
-  return <div>MenuNav</div>;
+  return (
+    <div>
+      <ul>
+        <li>Promotions</li>
+        <li>Lunch Specials</li>
+        <li>Daily Specials</li>
+        <li></li>
+      </ul>
+    </div>
+  );
 }
